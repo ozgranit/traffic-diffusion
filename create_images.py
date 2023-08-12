f6d507f3b661087c3b7cc0a9d402e07e34bee474
OzGranit-NICE <Oz.Granit@nice.com>
2023-08-12 19:02:43 +0300

    created ImageGenerator and prompt_getter to generate and save all transformations

--- create_images.py
+++ create_images.py
@@ -1,47 +1,78 @@
-from diffusers import DiffusionPipeline
 import torch
+
 from PIL import Image
+from pathlib import Path
+from prompts import prompt_getter
+from diffusers import DiffusionPipeline
 
-# Load both base & refiner
-base = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.float16,
-    variant="fp16",
-    use_safetensors=True
-)
 
-base.to("cuda")
-refiner = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    text_encoder_2=base.text_encoder_2,
-    vae=base.vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
-refiner.to("cuda")
+class ImageGenerator:
+    def __init__(self):
+        # Load both base & refiner
 
-# Define how many steps and what % of steps to be run on each experts (80/20) here
-n_steps = 40
-high_noise_frac = 0.8
+        base = DiffusionPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0",
+            torch_dtype=torch.float32,
+            variant="fp16",
+            use_safetensors=True
+        )
 
-prompt = "A majestic lion jumping from a big stone at night"
-path_to_image = r'/home/mansour/ozgranit/traffic-diffusion/kaggle_image/road96.png'  # Replace with the correct path to your image file
+        base.to("cuda")
+        self.refiner = DiffusionPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-refiner-1.0",
+            text_encoder_2=base.text_encoder_2,
+            vae=base.vae,
+            torch_dtype=torch.float32,
+            use_safetensors=True,
+            variant="fp16",
+        )
+        self.refiner.to("cuda")
 
-# Load the image using PIL
-image = Image.open(path_to_image)
-# Resize the image to 128x128
-desired_size = (128, 128)
-image_resized = image.resize(desired_size, Image.ANTIALIAS)
+    def generate_image(self, original_image, prompt):
+        # Define how many steps and what % of steps to be run on each expert (80/20) here
+        n_steps = 40
+        high_noise_frac = 0.8
 
-image = refiner(
-    prompt=prompt,
-    num_inference_steps=n_steps,
-    denoising_start=high_noise_frac,
-    image=image,
-).images[0]
+        new_image = self.refiner(
+            prompt=prompt,
+            num_inference_steps=n_steps,
+            denoising_start=high_noise_frac,
+            image=original_image,
+        ).images[0]
 
+        return new_image
+
+    def generate_variations(self, original_image, image_file_name, run_over_existing_images=False):
+
+        image_dir = Path.cwd() / 'dataset_images' / image_file_name
+        image_dir.mkdir(exist_ok=True)
+
+        for transformation, prompt in prompt_getter.items():
+            transformed_image = self.generate_image(original_image, prompt)
+
+            transformed_image_path = image_dir / f'{transformation}.png'
+
+            if transformed_image_path.exists() and not run_over_existing_images:
+                continue
+
+            transformed_image.save(transformed_image_path)
+
+    def generate_all_images(self):
+
+        original_images_path = Path.cwd() / 'kaggle_images'
+
+        # Iterate over files and directories in the kaggle_images directory
+        for item in original_images_path.iterdir():
+
+            if item.is_file():
+                # Load the image using PIL
+                original_image = Image.open(item.resolve())
+                self.generate_variations(original_image, item.name)
+
+            else:
+                raise IsADirectoryError("Should only find images in the kaggle_images directory")
 
 
 if __name__ == "__main__":
-    image.save('/home/mansour/ozgranit/traffic-diffusion/kaggle_image/new.png')
+    image_gen = ImageGenerator()
+    image_gen.generate_all_images()
---
+++ prompts.py
@@ -0,0 +1,11 @@
+"""a file to hold all our working prompts"""
+
+prompt_getter = {
+    'rain': "A stop sign on a rainy day",
+    'night': "A stop sign at night",
+    'snow': "A stop sign in the snow",
+    'dawn': "A stop sign at the early morning, dawn",
+    'cloud': "A stop sign on a cloudy day",
+    'midday': "A stop sign on midday, high sun",
+}
+
