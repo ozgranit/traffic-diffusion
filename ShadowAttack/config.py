b8d7b1d153d162d8ee98f489a0c97ec67c431715
ortal <ortallevi@mail.tau.ac.il>
2023-08-30 14:09:11 +0300

    Commit Shadow attack with our changes

---
+++ ShadowAttack/config.py
@@ -0,0 +1,9 @@
+MODEL_PATH = 'ShadowAttack/model'
+PARAMS_PATH = 'ShadowAttack/params.json'
+MASK_FOLDER = 'ShadowAttack/mask'
+
+# MODEL_PATH = 'model'
+# PARAMS_PATH = 'params.json'
+# MASK_FOLDER = 'mask'
+
+
