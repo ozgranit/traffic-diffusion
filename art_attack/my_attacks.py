1152cffe5c0c32dd805faaeb9b03ed70949102a2
OzGranit-NICE <Oz.Granit@nice.com>
2023-09-11 19:25:02 +0300

    method b works

---
+++ art_attack/my_attacks.py
@@ -0,0 +1,263 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PGDAttack:
+    """
+    White-box L_inf PGD attack using the cross-entropy loss
+    """
+    def __init__(self, model, eps=8/255., n=50, alpha=1/255.,
+                 rand_init=True, early_stop=True):
+        """
+        Parameters:
+        - model: model to attack
+        - eps: attack's maximum norm
+        - n: max # attack iterations
+        - alpha: step size at each iteration
+        - rand_init: a flag denoting whether to randomly initialize
+              adversarial samples in the range [x-eps, x+eps]
+        - early_stop: a flag denoting whether to stop perturbing a
+              sample once the attack goal is met. If the goal is met
+              for all samples in the batch, then the attack returns
+              early, before completing all the iterations.
+        """
+        self.model = model
+        self.n = n
+        self.alpha = alpha
+        self.eps = eps
+        self.rand_init = rand_init
+        self.early_stop = early_stop
+        self.loss_func = nn.CrossEntropyLoss(reduction='none')
+
+    def execute(self, x, y, targeted=False):
+        """
+        Executes the attack on a batch of samples x. y contains the true labels
+        in case of untargeted attacks, and the target labels in case of targeted
+        attacks. The method returns the adversarially perturbed samples, which
+        lie in the ranges [0, 1] and [x-eps, x+eps]. The attack optionally
+        performs random initialization and early stopping, depending on the
+        self.rand_init and self.early_stop flags.
+        """
+        adv_x = x.clone().detach()
+        if self.rand_init:
+            # Starting at a uniformly random point
+            adv_x = adv_x + torch.empty_like(adv_x).uniform_(-self.eps, self.eps)
+            adv_x = torch.clamp(adv_x, min=0, max=1).detach()
+
+        for i in range(self.n):
+            adv_x.requires_grad = True
+            outputs = self.model(adv_x)
+            if self.early_stop:
+                if not targeted and not (torch.argmax(outputs, dim=1) == y).any():
+                    break
+                elif targeted and (torch.argmax(outputs, dim=1) == y).all():
+                    break
+
+            # Calculate loss
+            loss = self.loss_func(outputs, y)
+            if targeted:
+                loss = -loss
+            # Update adversarial images
+            grad = torch.autograd.grad(loss.sum(), adv_x, retain_graph=False, create_graph=False)[0]
+
+            adv_x = adv_x.detach() + self.alpha * grad.sign()
+            delta = torch.clamp(adv_x - x, min=-self.eps, max=self.eps)
+            adv_x = torch.clamp(x + delta, min=0, max=1).detach()
+
+        assert torch.all(adv_x >= 0.) and torch.all(adv_x <= 1.)
+        assert torch.all(torch.abs(adv_x - x) <= self.eps + 1e-7)
+
+        return adv_x
+
+
+class NESBBoxPGDAttack:
+    """
+    Query-based black-box L_inf PGD attack using the cross-entropy loss,
+    where gradients are estimated using Natural Evolutionary Strategies
+    (NES).
+    """
+    def __init__(self, model, eps=8/255., n=50, alpha=1/255., momentum=0.,
+                 k=200, sigma=1/255., rand_init=True, early_stop=True):
+        """
+        Parameters:
+        - model: model to attack
+        - eps: attack's maximum norm
+        - n: max # attack iterations
+        - alpha: PGD's step size at each iteration
+        - momentum: a value in [0., 1.) controlling the "weight" of
+             historical gradients estimating gradients at each iteration
+        - k: the model is queries 2*k times at each iteration via
+              antithetic sampling to approximate the gradients
+        - sigma: the std of the Gaussian noise used for querying
+        - rand_init: a flag denoting whether to randomly initialize
+              adversarial samples in the range [x-eps, x+eps]
+        - early_stop: a flag denoting whether to stop perturbing a
+              sample once the attack goal is met. If the goal is met
+              for all samples in the batch, then the attack returns
+              early, before completing all the iterations.
+        """
+        self.model = model
+        self.eps = eps
+        self.n = n
+        self.alpha = alpha
+        self.momentum = momentum
+        self.k = k
+        self.sigma = sigma
+        self.rand_init = rand_init
+        self.early_stop = early_stop
+        self.loss_func = nn.CrossEntropyLoss(reduction='none')
+
+    def nes_gradient_estimate(self, adv_x, x, y, targeted):
+        grad = torch.zeros_like(adv_x)
+        query_count = 0
+
+        for j in range(self.n):
+            adv_x.requires_grad = True
+            outputs = self.model(adv_x)
+            if self.early_stop:
+                if not targeted and not (torch.argmax(outputs, dim=1) == y).any():
+                    break
+                elif targeted and (torch.argmax(outputs, dim=1) == y).all():
+                    break
+
+            grad_estimates = torch.zeros_like(adv_x)
+            for i in range(self.k):
+                delta_i = torch.normal(mean=0, std=1, size=adv_x.shape)  # sample from N(0, I_NxN)
+                theta_i = adv_x.detach() + self.sigma * delta_i
+                minus_theta_i = adv_x.detach() - self.sigma * delta_i
+                plus_output = self.model(theta_i)
+                minus_output = self.model(minus_theta_i)
+
+                # Calculate loss
+                plus_loss = self.loss_func(plus_output, y)
+                minus_loss = self.loss_func(minus_output, y)
+                if targeted:
+                    plus_loss = -plus_loss
+                    minus_loss = -minus_loss
+
+                grad_estimates += delta_i * plus_loss.sum()
+                grad_estimates -= delta_i * minus_loss.sum()
+
+            # Normalize the gradient estimate.
+            grad_estimates /= (2 * self.k * self.sigma)
+            # Update the historical gradient.
+            grad = self.momentum * grad + (1 - self.momentum) * grad_estimates
+
+            query_count += 2 * self.k
+            adv_x = adv_x.detach() + self.alpha * grad.sign()
+            delta = torch.clamp(adv_x - x, min=-self.eps, max=self.eps).detach()
+            adv_x = torch.clamp(x + delta, min=0, max=1).detach()
+
+        return adv_x, query_count
+
+    def execute(self, x, y, targeted=False):
+        """
+        Executes the attack on a batch of samples x. y contains the true labels
+        in case of untargeted attacks, and the target labels in case of targeted
+        attacks. The method returns:
+        1- The adversarially perturbed samples, which lie in the ranges [0, 1]
+            and [x-eps, x+eps].
+        2- A vector with dimensionality len(x) containing the number of queries for
+            each sample in x.
+        """
+        adv_x = x.clone().detach()
+        queries_per_sample = torch.zeros(len(x), dtype=torch.int32)
+
+        if self.rand_init:
+            # Starting at a uniformly random point
+            adv_x = adv_x + torch.empty_like(adv_x).uniform_(-self.eps, self.eps)
+            adv_x = torch.clamp(adv_x, min=0, max=1).detach()
+
+        for i in range(len(x)):
+            xi = x[i, ...][None, ...]
+            adv_xi = adv_x[i, ...][None, ...]
+            yi = y[i, ...][None, ...]
+
+            adv_xi, query_count = self.nes_gradient_estimate(adv_xi, xi, yi, targeted)
+
+            queries_per_sample[i, ...][None, ...] = query_count
+            adv_x[i, ...][None, ...] = adv_xi.detach()
+
+        assert torch.all(adv_x >= 0.) and torch.all(adv_x <= 1.)
+        assert torch.all(torch.abs(adv_x - x) <= self.eps + 1e-7)
+
+        return adv_x, queries_per_sample
+
+
+class PGDEnsembleAttack:
+    """
+    White-box L_inf PGD attack against an ensemble of models using the
+    cross-entropy loss
+    """
+    def __init__(self, models, eps=8/255., n=50, alpha=1/255.,
+                 rand_init=True, early_stop=True):
+        """
+        Parameters:
+        - models (a sequence): an ensemble of models to attack (i.e., the
+              attack aims to decrease their expected loss)
+        - eps: attack's maximum norm
+        - n: max # attack iterations
+        - alpha: PGD's step size at each iteration
+        - rand_init: a flag denoting whether to randomly initialize
+              adversarial samples in the range [x-eps, x+eps]
+        - early_stop: a flag denoting whether to stop perturbing a
+              sample once the attack goal is met. If the goal is met
+              for all samples in the batch, then the attack returns
+              early, before completing all the iterations.
+        """
+        self.models = models
+        self.n = n
+        self.alpha = alpha
+        self.eps = eps
+        self.rand_init = rand_init
+        self.early_stop = early_stop
+        self.loss_func = nn.CrossEntropyLoss()
+
+    def execute(self, x, y, targeted=False):
+        """
+        Executes the attack on a batch of samples x. y contains the true labels
+        in case of untargeted attacks, and the target labels in case of targeted
+        attacks. The method returns the adversarially perturbed samples, which
+        lie in the ranges [0, 1] and [x-eps, x+eps].
+        """
+        adv_x = x.clone().detach()
+        if self.rand_init:
+            # Starting at a uniformly random point
+            adv_x = adv_x + torch.empty_like(adv_x).uniform_(-self.eps, self.eps)
+            adv_x = torch.clamp(adv_x, min=0, max=1).detach()
+
+        for i in range(self.n):
+            adv_x.requires_grad = True
+
+            # Calculate loss
+            loss = torch.tensor(0.)
+            for model in self.models:
+                outputs = model(adv_x)
+                loss += self.loss_func(outputs, y)
+            if targeted:
+                loss = -loss
+            # Update adversarial images
+            grad = torch.autograd.grad(loss, adv_x, retain_graph=False, create_graph=False)[0]
+
+            adv_x = adv_x.detach() + self.alpha * grad.sign()
+            delta = torch.clamp(adv_x - x, min=-self.eps, max=self.eps)
+            adv_x = torch.clamp(x + delta, min=0, max=1).detach()
+
+            # Check if attack goal is met
+            with torch.no_grad():
+                all_correct = True
+                for model in self.models:
+                    output = model(adv_x)
+                    preds = output.argmax(dim=1)
+                    if targeted:
+                        all_correct = all_correct and (preds == y).all().item()
+                    else:
+                        all_correct = all_correct and (preds != y).all().item()
+                if all_correct and self.early_stop:
+                    break
+
+        assert torch.all(adv_x >= 0.) and torch.all(adv_x <= 1.)
+        assert torch.all(torch.abs(adv_x - x) <= self.eps + 1e-7)
+
+        return adv_x
--- pgd_attack.py
+++ pgd_attack.py
@@ -1,3 +1,4 @@
+import cv2
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,9 +12,16 @@ from art_attack.image_getter import image_generator
 def plot_images(images, labels, nrows, ncols):
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 10))
     for i, ax in enumerate(axes.flatten()):
-        ax.imshow(images[i])
+        ax.imshow(cv2.cvtColor(images[i], cv2.COLOR_BGR2RGB))
         ax.set_title(f"Label: {np.argmax(labels[i])}")
         ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_image(image, label):
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(f"Label: {np.argmax(label)}")
     plt.tight_layout()
     plt.show()
 
@@ -39,20 +47,55 @@ def load_lisa_model():
     return model
 
 
-def main():
-    model = load_lisa_model()
+def method_b(pgd_attack, model):
+    count = 0
+    test_accuracy, attacked_test_accuracy = 0, 0
+
+    for orig_image, train_images, test_images in image_generator(size=SIZE):
+
+        count += 1
+        attacks = []
+        label = torch.tensor([12])
+
+        for train_image in [orig_image] + train_images:
+
+            # Swap axes to PyTorch's NCHW format
+            input_image = [np.transpose(train_image, (2, 0, 1))]
+            input_image = torch.tensor(np.stack(input_image, axis=0)) / 255
+
+            x_adv = pgd_attack.execute(input_image, torch.tensor([11]), targeted=True)
+            attack_only = x_adv.detach().numpy() - input_image.detach().numpy()
+            attacks.append(attack_only)
+
+        final_attack = np.mean(attacks, axis=0)
+        # apply attack to test images
+        for test_image in test_images:
+            test_image = [np.transpose(test_image, (2, 0, 1))]
+            test_image = torch.tensor(np.stack(test_image, axis=0)) / 255
+
+            test_pred = model(test_image).detach().numpy()
+            test_accuracy += np.sum(np.argmax(test_pred, axis=1) == label.item()) / len(test_images)
+
+            attack_pred = model(test_image + final_attack).detach().numpy()
+            attacked_test_accuracy += np.sum(np.argmax(attack_pred, axis=1) == label.item()) / len(test_images)
+
+    print(f'Accuracy on test images: {test_accuracy * 100 / count}%')
+    print(f'Accuracy on EOT attacked test images: {attacked_test_accuracy * 100 / count}%')
+
+
+def method_a(pgd_attack, model):
+
     images_for_plot, labels_for_plot, adv_images_for_plot, adv_labels_for_plot = [], [], [], []
     orig_accuracy, adv_accuracy, count = 0, 0, 0
     test_accuracy, attacked_test_accuracy = 0, 0
 
-    for orig_image, train_images, test_images in image_generator():
+    for orig_image, train_images, test_images in image_generator(size=SIZE):
 
         # Swap axes to PyTorch's NCHW format
         input_image = [np.transpose(orig_image, (2, 0, 1))]
         input_image = torch.tensor(np.stack(input_image, axis=0)) / 255
         label = torch.tensor([12])
 
-        pgd_attack = PGDAttack(model=model, eps=40/255, n=100)
         x_adv = pgd_attack.execute(input_image, label)
 
         orig_predictions = model(input_image).detach().numpy()
@@ -88,5 +131,22 @@ def main():
     plot_images(imshow_batch(adv_images_for_plot), adv_labels_for_plot, nrows=5, ncols=5)
 
 
+SIZE = 224
+
 if __name__ == "__main__":
-    main()
+    # TODO: https://github.com/adverML/synthesizing_robust_adversarial/blob/main/main.ipynb
+    model = load_lisa_model()
+    pgd_attack = PGDAttack(model=model, eps=12/255, n=50)
+    # pgd_attack = PGDAttack(model=model, eps=12/255, n=50), baseline:
+    # Accuracy on benign examples: 96.0%
+    # Accuracy on adversarial examples: 64.0%
+    # Accuracy on test images: 99.33333333333348%
+
+    # Accuracy on attacked test images: 97.33333333333347%
+    # Accuracy on EOT attacked test images: 96.00000000000013%
+    # size 224:
+    # Accuracy on attacked test images: 97.33333333333347%
+    # Accuracy on EOT attacked test images: 96.00000000000013%
+
+    method_a(pgd_attack, model)
+    method_b(pgd_attack, model)
