9281192f664e93de5877caa1ce17f75e8f092488
ortal <ortallevi@mail.tau.ac.il>
2024-02-12 20:20:04 +0200

    stable diffusion locally params

---
+++ diffusionImageGenerator/locally/stable_diffusion_xl_params.py
@@ -0,0 +1,9 @@
+
+
+def stable_diffusion_xl_main_params():
+    data = {}
+    data['strength'] = 0.45  #0.7
+    data['num_inference_steps'] = 30
+    data['guidance_scale'] = 20
+
+    return data
