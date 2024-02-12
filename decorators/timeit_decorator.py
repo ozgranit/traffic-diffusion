8ef58b98171bd3233ce5e9980676e4df6ed7f3c7
ortal <ortallevi@mail.tau.ac.il>
2024-02-12 20:08:27 +0200

    Adding timeit decorator

---
+++ decorators/timeit_decorator.py
@@ -0,0 +1,12 @@
+import time
+
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__} took {elapsed_time} seconds to execute.")
+        return result
+    return wrapper
