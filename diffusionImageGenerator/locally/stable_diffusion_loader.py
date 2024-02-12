ee66549f2e503ef42a542ebf83d35c7d8a6e32c7
ortal <ortallevi@mail.tau.ac.il>
2024-02-12 20:17:02 +0200

    stable diffusion loader locally (weights ares loaded from huggingFace)

---
+++ diffusionImageGenerator/locally/stable_diffusion_loader.py
@@ -0,0 +1,12 @@
+import torch
+from diffusers import StableDiffusionXLImg2ImgPipeline
+
+
+def load_stable_diffusion_xl():
+    pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16
+    )
+    pipe = pipe.to("cuda")
+
+    return pipe
+
